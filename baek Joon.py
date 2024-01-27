4263d593b6d221810be554e6a9fce6f0f22ec9b7
tthungjun <150824735+tthungjun@users.noreply.github.com>
2024-01-27 23:01:11 +0900

    Add files via upload

---
+++ baek Joon.py
@@ -0,0 +1,49 @@
+'''year = int(input())
+
+if (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
+    print("1")
+else:
+    print('0')'''
+
+'''x = int(input())
+y = int(input())
+
+if (x > 0) and (y > 0):
+    print("1")
+elif (x < 0) and (y > 0):
+    print("2")
+elif (x < 0) and (y < 0):
+    print("3")
+elif (x > 0) and (y < 0):
+    print("4")'''
+
+'''H = int(input())
+M = int(input())
+
+if (M >= 45 and M < 60):
+    print(H, M-45)
+elif (M < 45 and M >=0):
+    if H >=1 and H <=23:
+        print(H, M + 15)
+    elif H == 0:
+        print(23, M + 15)'''
+
+'''H = int(input())
+M = int(input())
+C = int(input())
+
+if M + C <60:
+    print(H, M+C)
+elif M + C >= 60:'''
+    
+# N = int(input())
+
+# for i in range(1, 10):
+#     print(f"{N} * {i} =",N*i)
+
+a = int(input())
+b = list(map(int, input(). split()))
+c = int(input)
+
+print(b. count(c))
+
