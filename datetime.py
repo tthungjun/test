875377bcd5a212ce6ae7599c8715dd0218512e62
tthungjun <150824735+tthungjun@users.noreply.github.com>
2024-02-04 23:58:03 +0900

    Add files via upload

---
+++ datetime.py
@@ -0,0 +1,14 @@
+import datetime
+now = datetime. datetime. now()
+현재시간 = now. hour
+
+입력 = input("입력: ")
+if "안녕" in 입력:
+    print("안녕하세요.")
+elif "안녕하세요" in 입력:
+    print("안녕하세요.")
+
+if "지금 몇 시야?" in  입력:
+    print("지금은 {}시입니다". format(now. hour))
+elif "지금 몇 시예요?" in  입력:
+    print("지금은 {}시입니다". format(now. hour))
