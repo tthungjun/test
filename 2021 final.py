a2a6f33b5c79c0dc929472b89fd81d8dc575e68b
tthungjun <150824735+tthungjun@users.noreply.github.com>
2024-02-08 21:57:50 +0900

    Add files via upload

---
+++ 2021 final.py
@@ -0,0 +1,22 @@
+'''class City:
+    def __init__(self, name, population, area):
+        self. name = name
+        self. population = population
+        self. area = area
+    def __str__(self):
+        return f''
+    def density(self):
+        return self. population / self. area
+    
+class Country(City):
+    def __init__(self, name, cities = []):
+        self. name = name
+        self. cities = cities
+    def num_cities(self):
+        return len(self. cities)
+    def total_population(self):
+        return 
+    def total_area(self):
+    def density(self):
+    def __str__(self):'''
+
